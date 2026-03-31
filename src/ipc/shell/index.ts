7ce2db58947d28893348f3cd75de0959a0748a2d
Shawan Mandal <shawan.sm049@gmail.com>
2026-03-31 22:45:23 +0530

    refactor: code cleanup for unused code and modules

--- src/ipc/shell/index.ts
+++ src/ipc/shell/index.ts
@@ -1,9 +1,5 @@
 import { openExternalLink } from "./handlers";
-import { downloadVideo, downloadPlaylist, downloadBulk } from "./download-handlers";
 
 export const shell = {
   openExternalLink,
-  downloadVideo,
-  downloadPlaylist,
-  downloadBulk,
 };
